a57301a1c133f274e2d8c5b4be55942f29f3378d
Aditya Rathore <vishurat531@gmail.com>
2025-09-04 21:49:44 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./Components/Layout";
 import HomePage from "./Pages/HomePage";
 import Visuals from "./Pages/Visuals";
 
