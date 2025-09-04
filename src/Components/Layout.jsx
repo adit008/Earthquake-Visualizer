e7c6745ecee15aaeabb892f625ed5af12e7849ea
Aditya Rathore <vishurat531@gmail.com>
2025-09-04 21:38:57 +0530

    final commit

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,7 +2,7 @@ import { Outlet, Link } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className="h-screen w-screen flex flex-col overflow-hidden">
+    <div className="h-screen w-screen flex flex-col overflow-x-hidden overflow-y-auto">
       {/* Fixed Navbar */}
       <nav className="sticky top-0 left-0 w-full bg-zinc-900 text-white shadow-md z-50 flex justify-between items-center px-8 py-4">
         <div className="text-xl font-bold text-center flex-1">
@@ -19,7 +19,7 @@ function Layout() {
       </nav>
 
       {/* Main Content */}
-      <main className="flex-1 pt-20 overflow-hidden">
+      <main className="flex-1 overflow-y-auto overflow-x-hidden">
         <Outlet />
       </main>
     </div>
